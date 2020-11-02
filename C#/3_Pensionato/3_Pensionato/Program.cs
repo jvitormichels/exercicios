6f249fa83652d59f327ee1e4df74abb39db0bfac
Michels <71910236+jvitormichels@users.noreply.github.com>
2020-11-02 19:24:18 -0300

    exercicio do pensionato completo
    registro de ocupantes em um vetor de quartos com relatório de quartos ocupados

---
+++ C#/3_Pensionato/3_Pensionato/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _3_Pensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quarto[] quartos = new Quarto[10];
+            Console.WriteLine("Quantos quartos serão alugados?");
+            int quartosAlugados = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quartosAlugados; i++)
+            {
+                Console.Write("Informe o número do quarto: ");
+                int nQuarto = int.Parse(Console.ReadLine());
+                Console.Write("Informe seu nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Informe seu email: ");
+                string email = Console.ReadLine();
+
+                quartos[nQuarto] = new Quarto(nome, email, nQuarto);
+            }
+
+
+            Console.WriteLine(" --- Relatório de quartos ---\n");
+            for (int i = 0; i < 10; i++)
+            {
+                if (quartos[i] != null)
+                {
+                    Console.WriteLine(quartos[i]);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ C#/3_Pensionato/3_Pensionato/Quarto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_Pensionato
+{
+    class Quarto
+    {
+        public int NumeroQuarto;
+        public string OcupanteNome;
+        public string OcupanteEmail;
+
+        public Quarto(string ocupanteNome, string ocupanteEmail, int numero)
+        {
+            NumeroQuarto = numero;
+            OcupanteNome = ocupanteNome;
+            OcupanteEmail = ocupanteEmail;
+        }
+
+        public override string ToString()
+        {
+            return $"--- Quarto n°{NumeroQuarto} ---\n" +
+                $"Ocupante: {OcupanteNome}, {OcupanteEmail}";
+        }
+    }
+}
