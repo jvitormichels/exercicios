e04ff20474945e1f25efeb65425d349d25368b92
Michels <71910236+jvitormichels@users.noreply.github.com>
2020-11-02 10:49:46 -0300

    files added

---
+++ C#/1_EstoqueSimples/EstoqueSimples/Produto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace EstoqueSimples
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _quantidade * _preco;
+        }
+
+        public void AdicionaProduto(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoveProduto(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"Dados do produto \n" +
+                $"----------------\n" +
+                $"Produto: {_nome} \n" +
+                $"Preço: R${_preco.ToString("F2", CultureInfo.InvariantCulture)} \n" +
+                $"Em estoque: {_quantidade} \n" +
+                $"Valor em estoque: {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ C#/1_EstoqueSimples/EstoqueSimples/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace EstoqueSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.WriteLine("Entre com as informaçõess abaixo.");
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço do produto: R$");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade do produto: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Produto produto1 = new Produto(nome, preco, quantidade);
+            Console.WriteLine(produto1);
+            Console.WriteLine("\n");
+
+            Console.Write("Informe a quantidade a ser adicionada ao estoque: ");
+            produto1.AdicionaProduto(int.Parse(Console.ReadLine()));
+            Console.WriteLine();
+            Console.WriteLine(produto1);
+            Console.WriteLine("\n");
+
+            Console.Write("Informe a quantidade a ser removida do estoque: ");
+            produto1.RemoveProduto(int.Parse(Console.ReadLine()));
+            Console.WriteLine();
+            Console.WriteLine(produto1);
+            Console.WriteLine("\n");
+
+        }
+
+    }
+}
