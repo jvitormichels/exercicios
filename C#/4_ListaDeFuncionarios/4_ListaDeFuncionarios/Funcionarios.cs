eaf2d9ba70b4c5c33902196349bf7a94b8397669
Michels <71910236+jvitormichels@users.noreply.github.com>
2020-11-17 00:05:32 -0300

    lista de objetos funcionario
    exercicio de fixação de listas

---
+++ C#/4_ListaDeFuncionarios/4_ListaDeFuncionarios/Funcionarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace _4_ListaDeFuncionarios
+{
+    class Funcionarios
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+
+        public Funcionarios(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentoSalario(double porcentagemAumento)
+        {
+            Salario = Salario * ((porcentagemAumento / 100) + 1);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, R${Salario}";
+        }
+    }
+}
---
+++ C#/4_ListaDeFuncionarios/4_ListaDeFuncionarios/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4_ListaDeFuncionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionarios> funcionarios = new List<Funcionarios>();
+            char escolha;
+            do
+            {
+                Console.WriteLine("O que deseja fazer? \n1.Adicionar um novo funcionário \n2.Aumentar o salário de um funcionário \n3.Listar os funcionários \n4.Sair");
+                escolha = char.Parse(Console.ReadLine());
+                Console.Clear();
+
+                switch (escolha)
+                {
+                    case '1':
+                        Console.WriteLine($"Informe os dados do funcionário: ");
+                        Console.Write("ID: ");
+                        int id = int.Parse(Console.ReadLine());
+                        Console.Write("Nome: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("Salário: ");
+                        double salario = double.Parse(Console.ReadLine());
+                        Console.Clear();
+                        funcionarios.Add(new Funcionarios(id, nome, salario));
+                        break;
+
+                    case '2':
+                        Console.Write("Informe o ID do funcionário: ");
+                        int idPesquisado = int.Parse(Console.ReadLine());
+                        Funcionarios aux = funcionarios.Find(x => x.Id == idPesquisado);
+                        if (aux == null)
+                        {
+                            Console.WriteLine("Id inválido!");
+                        }
+                        else
+                        {
+                            Console.Write("Informe a porcentagem: ");
+                            double porcentagem = double.Parse(Console.ReadLine());
+                            aux.AumentoSalario(porcentagem);
+                        }
+                        break;
+
+                    case '3':
+                        foreach (Funcionarios obj in funcionarios)
+                        {
+                            Console.WriteLine(obj);
+                        }
+                        break;
+                }
+            } while (escolha != '4');
+        }
+    }
+}
