586ff4a7b0b1bc96aaccfde06d681fb89763b0fb
Michels <jvitormichels@gmail.com>
2021-01-22 09:03:33 -0300

    exercicio simples de tratamento de exceções

---
+++ C#/reservaDeHotel/reservaDeHotel/Entities/Reservation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using reservaDeHotel.Entities.Exceptions;
+
+namespace reservaDeHotel.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkIn > checkOut)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration ()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.Days;
+        }
+
+        public void UpdateDates (DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || CheckOut < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            if (checkIn > checkOut)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return $"Reservation: Room {RoomNumber}, Checkin: {CheckIn.ToString("dd/MM/yyyy")}, Checkout: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights";
+        }
+    }
+}
---
+++ C#/reservaDeHotel/reservaDeHotel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using reservaDeHotel.Entities;
+using reservaDeHotel.Entities.Exceptions;
+
+namespace reservaDeHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date: ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date: ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+                Reservation r = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine(r);
+
+                Console.WriteLine("\nEnter data to update the reservation: ");
+                Console.Write("Check-in date: ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date: ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+                r.UpdateDates(checkIn, checkOut);
+                Console.WriteLine(r);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error! " + e.Message);
+            }
+        }
+    }
+}
